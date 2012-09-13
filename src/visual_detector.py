7bde631f6e9efa18f88cfd5eeadd84f9decc2007
Narcis Palomeras <narcispr@gmail.com>
2012-09-13 16:38:24 +0200

    Add geometric detector.

--- src/visual_detector.py
+++ src/visual_detector.py
@@ -6,15 +6,19 @@ roslib.load_manifest('udg_pandora')
 import rospy
 import std_msgs.msg
 import std_srvs.srv
+from auv_msgs.msg import NavSts
 from udg_pandora.msg import Detection
 import metaclient
+from detector import GeometricDetector
 
 class VisualDetector:
     def __init__(self, name):
         self.name = name
+        self.geometric_detector = GeometricDetector('geometric_detector')
         
         # Create Subscriber
         sub = metaclient.Subscriber("VisualDetectorInput", std_msgs.msg.Empty, {}, self.updateImage) 
+        nav = metaclient.Subscriber("/cola2_navigation/nav_sts", NavSts, {}, self.updateNavigation)
         
         # Create publisher
         self.pub_valve_panel = metaclient.Publisher('/visual_detector/valve_panel', Detection,{})
@@ -57,6 +61,18 @@ class VisualDetector:
     def updateImage(self, img):
         pass
     
+    def updateNavigation(self, nav):
+        vehicle_pose = [nav.position.north, nav.position.east, nav.position.depth]
+        vehicle_orientation = [nav.orientation.roll, nav.orientation.pitch, nav.orientation.yaw]
+        d = self.geometric_detector.detectObjects(vehicle_pose, vehicle_orientation)
+        for i in range(len(d)):
+            if d[i].detected:       
+                if i == 0:
+                    self.pub_valve_panel.publish(d[i])
+                elif i == 1:
+                    self.pub_valve.publish(d[i])
+                else:
+                    self.pub_chain.publish(d[i])
     
 if __name__ == '__main__':
     try:
