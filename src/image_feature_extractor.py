4296d0163c9fa97c6f04f5aeaf9b4914b97f84b8
snagappa <git@sharad.nagappa.org>
2012-09-25 16:03:23 +0200

    Rotated the virtual camera and changed its resolution. Added support for flann based matching using opencv 2.3 flann_Index() interface.

--- src/image_feature_extractor.py
+++ src/image_feature_extractor.py
@@ -8,15 +8,19 @@ Created on Mon Sep 10 13:33:17 2012
 import roslib
 roslib.load_manifest('udg_pandora')
 import cv2
-import collections
+#import collections
 
+class DETECTOR_PRESET:
+    FINE, MED, COARSE = range(3)
+
+"""
 SURF_PARAMS = collections.namedtuple("SURF_PARAMS", ['hessianThreshold', 
                                                      'nOctaves', 
                                                      'nOctaveLayers',
                                                      'extended',
                                                      'upright'])
 ORB_PARAMS = collections.namedtuple("ORB_PARAMS", ['threshold'])
-
+"""
 class _feature_detector_(object):
     def __init__(self):
         self.PARAMS = None
@@ -34,16 +38,37 @@ class _feature_detector_(object):
         
 
 class surf(object):
-    def __init__(self, minHessian=400):
-        PARAMS = SURF_PARAMS(minHessian, 4, 2, 1, False)
+    def __init__(self, PRESET=DETECTOR_PRESET.MED):
+        hessianThreshold = 400
+        nOctaves = 4
+        nOctaveLayers = 2
+        extended = 1
+        upright = False
+        if PRESET == DETECTOR_PRESET.FINE:
+            hessianThreshold = 200
+        elif PRESET == DETECTOR_PRESET.MED:
+            hessianThreshold = 400
+        else:
+            hessianThreshold = 800
+        #PARAMS = SURF_PARAMS(minHessian, nOctaves, nOctaveLayers, 
+        #                     extended, upright)
+        PARAMS = dict(hessianThreshold=hessianThreshold, 
+                      nOctaves=nOctaves,
+                      nOctaveLayers=nOctaveLayers,
+                      extended=extended,
+                      upright=upright)
         self.PARAMS = PARAMS
         self.NORM = cv2.NORM_L2
+        self.DESCRIPTOR_IS_BINARY = False
         self._keypoints_ = None
         self._descriptors_ = None
         self._detector_ = cv2.SURF(*PARAMS)
         self._detect_ = lambda im, mask: self._detector_.detectAndCompute(im, mask, 
             useProvidedKeypoints = False)
         
+    def reinit(self):
+        self._detector_ = cv2.SURF(**self.PARAMS)
+    
     def get_features(self, im, mask=None):
         (self._keypoints_, self._descriptors_) = \
             self._detect_(im, mask)
@@ -51,16 +76,30 @@ class surf(object):
     
 
 class orb(object):
-    def __init__(self, threshold=10000):
-        PARAMS = ORB_PARAMS(threshold)
+    def __init__(self, PRESET=DETECTOR_PRESET.MED):
+        if PRESET == DETECTOR_PRESET.FINE:
+            nfeatures = 1000
+            nlevels = 16
+        elif PRESET == DETECTOR_PRESET.MED:
+            nfeatures = 1000
+            nlevels = 12
+        else:
+            nfeatures = 500
+            nlevels = 8
+        #PARAMS = ORB_PARAMS(threshold)
+        PARAMS = dict(nfeatures=nfeatures, nlevels=nlevels)
         self.PARAMS = PARAMS
         self.NORM = cv2.NORM_HAMMING
+        self.DESCRIPTOR_IS_BINARY = True
         self._keypoints_ = None
         self._descriptors_ = None
-        self._detector_ = cv2.ORB(*PARAMS)
+        self._detector_ = cv2.ORB(**PARAMS)
         self._detect_ = lambda im, mask: self._detector_.detectAndCompute(im, mask, 
             useProvidedKeypoints = False)
         
+    def reinit(self):
+        self._detector_ = cv2.SURF(**self.PARAMS)
+    
     def get_features(self, im, mask=None):
         (self._keypoints_, self._descriptors_) = \
             self._detect_(im, mask)
