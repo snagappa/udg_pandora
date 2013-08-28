Fixed errors in computation of sigma points for unscented transform. Added new functions to cameramodels for using disparity observation model. Fixed errors in PHD update using unscented transform.

src/lib/cameramodels.py
@@ -14,7 +14,6 @@ import image_feature_extractor
 from misctools import STRUCT, rotation_matrix, FlannMatcher, sample_mn_cv
 import cv2
 import copy
-import code
 from geometry_msgs.msg import PointStamped
 import sys
 import traceback
@@ -485,6 +484,48 @@ class PinholeCameraModel(ros_cameramodels.PinholeCameraModel, _FoV_):
     def get_tf_frame(self):
         return (self.tfFrame,)
     
+    def observation_px_jacobian(self, states):
+        num_states = states.shape[0]
+        proj_matrix = self.projection_matrix()
+        f = proj_matrix[0, 0]
+        rot_matrix = self.inv_rotation_matrix()
+        rot_states = np.dot(rot_matrix, states.T).T
+        # Need to divide by new (rotated) z to get image points
+        zz = rot_states[:, 2]
+        zz_sq = zz**2
+        f_zz_sq = f/zz_sq
+        # Create emtpy Jacobian
+        J_matrix = np.empty((num_states, 3, 3))
+        # Set last row to zero for all states
+        J_matrix[:, 2, :] = 0
+        # First row
+        J_matrix[:, 0, 0] = f_zz_sq*(rot_matrix[0, 0]*zz - rot_matrix[2, 0]*rot_states[:, 0])
+        J_matrix[:, 0, 1] = f_zz_sq*(rot_matrix[0, 1]*zz - rot_matrix[2, 1]*rot_states[:, 0])
+        J_matrix[:, 0, 2] = f_zz_sq*(rot_matrix[0, 2]*zz - rot_matrix[2, 2]*rot_states[:, 0])
+        # Second row
+        J_matrix[:, 1, 0] = f_zz_sq*(rot_matrix[1, 0]*zz - rot_matrix[2, 0]*rot_states[:, 1])
+        J_matrix[:, 1, 1] = f_zz_sq*(rot_matrix[1, 1]*zz - rot_matrix[2, 1]*rot_states[:, 1])
+        J_matrix[:, 1, 2] = f_zz_sq*(rot_matrix[1, 2]*zz - rot_matrix[2, 2]*rot_states[:, 1])
+        
+        return J_matrix
+#        u = proj_matrix[0, 2]
+#        v = proj_matrix[1, 2]
+#        J_matrix = self.inv_rotation_matrix()
+#        J_matrix[:2, :2] *= f
+#        J_matrix[0, :] += u*J_matrix[2, :]
+#        J_matrix[1, :] += v*J_matrix[2, :]
+#        J_matrix[:, 2] = 0
+#        z = states[:, 2, np.newaxis, np.newaxis] # (num_states x 1 x 1 ndarray)
+#        J_matrix = J_matrix/z
+#        return J_matrix
+#        
+#        #rot_matrix[0, :] *= proj_matrix[0, 0]
+#        rot_matrix[:, 0, :]
+#        rot_matrix[0, :] += proj_matrix[0, 2]/f*rot_matrix[2, :]
+#        #rot_matrix[1, :] *= proj_matrix[1, 1]
+#        rot_matrix[1, :] += proj_matrix[1, 2]/f*rot_matrix[2, :]
+#        return rot_matrix
+        
 
 class StereoCameraModel(ros_cameramodels.StereoCameraModel, _FoV_):
     def __init__(self):
@@ -637,8 +678,13 @@ class StereoCameraModel(ros_cameramodels.StereoCameraModel, _FoV_):
     def get_tf_frame(self):
         return (self.left.tfFrame, self.right.tfFrame)
     
-    def observations(self, states):
+    def observations(self, states, idx=None):
         rel_states = self.from_world_coords(states)
+        assert idx in [0, 1], "idx must be 0 or 1"
+        if idx == 0:
+            rel_states = rel_states[0]
+        elif idx == 1:
+            rel_states = rel_states[1]
         return rel_states
     
     def observations_px(self, states):
@@ -651,6 +697,62 @@ class StereoCameraModel(ros_cameramodels.StereoCameraModel, _FoV_):
             disparity_obs = np.empty((0, 3))
         return disparity_obs
     
+    def observation_px_jacobian_full(self, states):
+        num_states = states.shape[0]
+        proj_matrix_l, proj_matrix_r = self.projection_matrix()
+        
+        f = proj_matrix_l[0, 0]
+        baseline = proj_matrix_r[0, 3]
+        rot_matrix = self.inv_rotation_matrix()
+        rot_states = np.dot(rot_matrix, states.T).T
+        # Need to divide by new (rotated) z to get image points
+        zz = rot_states[:, 2]
+        zz_sq = zz**2
+        f_zz_sq = f/zz_sq
+        # Create emtpy Jacobian
+        J_matrix = np.empty((num_states, 3, 3))
+        # First row
+        J_matrix[:, 0, 0] = f_zz_sq*(rot_matrix[0, 0]*zz - rot_matrix[2, 0]*rot_states[:, 0])
+        J_matrix[:, 0, 1] = f_zz_sq*(rot_matrix[0, 1]*zz - rot_matrix[2, 1]*rot_states[:, 0])
+        J_matrix[:, 0, 2] = f_zz_sq*(rot_matrix[0, 2]*zz - rot_matrix[2, 2]*rot_states[:, 0])
+        # Second row
+        J_matrix[:, 1, 0] = f_zz_sq*(rot_matrix[1, 0]*zz - rot_matrix[2, 0]*rot_states[:, 1])
+        J_matrix[:, 1, 1] = f_zz_sq*(rot_matrix[1, 1]*zz - rot_matrix[2, 1]*rot_states[:, 1])
+        J_matrix[:, 1, 2] = f_zz_sq*(rot_matrix[1, 2]*zz - rot_matrix[2, 2]*rot_states[:, 1])
+        # Third row
+        J_matrix[:, 2, :] = baseline/zz_sq[:,np.newaxis]*rot_matrix[2, :]
+        
+        return J_matrix
+    
+    def observation_px_jacobian_partial(self, states):
+        num_states = states.shape[0]
+        proj_matrix_l, proj_matrix_r = self.projection_matrix()
+        
+        f = proj_matrix_l[0, 0]
+        baseline = proj_matrix_r[0, 3]
+        rot_matrix = self.inv_rotation_matrix()
+        #rot_states = np.dot(rot_matrix, states.T).T
+        # Need to divide by new (rotated) z to get image points
+        zz = states[:, 2]
+        zz_sq = zz**2
+        f_zz = f/zz
+        #f_zz_sq = f/zz_sq
+        # Create emtpy Jacobian
+        J_matrix = np.zeros((num_states, 3, 3))
+        # First row
+        J_matrix[:, 0, 0] = f_zz*rot_matrix[0, 0]
+        J_matrix[:, 0, 1] = f_zz*rot_matrix[0, 1]
+        
+        # Second row
+        J_matrix[:, 1, 0] = f_zz*rot_matrix[1, 0]
+        J_matrix[:, 1, 1] = f_zz*rot_matrix[1, 1]
+        
+        # Third row
+        J_matrix[:, 2, :] = baseline/zz_sq[:,np.newaxis]
+        
+        return J_matrix
+    
+
 class _CameraFeatureDetector_(object):
     def __init__(self, feature_extractor=image_feature_extractor.Orb, **kwargs):
         self._flann_matcher_ = None

src/lib/kalmanfilter.py
@@ -204,6 +204,52 @@ def ukf_predict(states, covs, ctrl_input, proc_noise, predict_fn,
     pred_cov = _sigma_cov_(sigma_x_pred, pred_state, wt_cv, 0)
     return pred_state, pred_cov
 
+
+def np_ukf_update_cov(state, cov, obs_noise, obsfn, obsfn_args=(), _alpha=1e-3, _beta=2, _kappa=0, INPLACE=True):
+    assert state.ndim == 1, "state must be 1D"
+    assert cov.ndim == 2, "cov must be 2D"
+    assert obs_noise.ndim == 2, "obs_noise must be 2D"
+    
+    kalman_info = lambda:0
+    
+    state_dim = state.shape[0]
+    num_sigma_pts = 2*state_dim + 1
+    
+    # Generate the sigma points
+    (x_sigma, wt_mn, wt_cv) = np_sigma_pts(state, cov, _alpha, _beta, _kappa)
+    # predicted mean x - same as current x?
+    predicted_x = (wt_mn[:, np.newaxis]*x_sigma).sum(axis=0)
+    
+    # Generate observations for predicted sigma points
+    z_sigma = np.ascontiguousarray(obsfn(x_sigma, *obsfn_args))
+    # predicted mean z
+    predicted_z = (wt_mn[:, np.newaxis]*z_sigma).sum(axis=0)
+    
+    # Innovation covariance - used to compute the likelihood
+    z_residual = z_sigma - predicted_z
+    zz_sigma_cov = np.asarray(
+        [wt_cv[count]*np.dot(z_residual[[count]].T, z_residual[[count]]) 
+        for count in range(num_sigma_pts)]).sum(axis=0) + obs_noise
+    
+    x_residual = x_sigma - predicted_x
+    xz_sigma_cov = np.asarray(
+        [wt_cv[count]*np.dot(x_residual[[count]].T, z_residual[[count]]) 
+        for count in range(num_sigma_pts)]).sum(axis=0)
+    
+    # Kalman gain
+    kalman_gain = np.dot(xz_sigma_cov, np.linalg.inv(zz_sigma_cov))
+    
+    cov_update_term = np.dot(kalman_gain, np.dot(zz_sigma_cov, kalman_gain.T))
+    if INPLACE:
+        cov -= cov_update_term
+    else:
+        cov = cov - cov_update_term
+    
+    kalman_info.S = zz_sigma_cov
+    kalman_info.kalman_gain = kalman_gain
+    return cov, predicted_z, kalman_info
+
+
 def ukf_update_cov(states, covs, obs_noise, obsfn, obsfn_args=(), _alpha=1e-3, _beta=2, _kappa=0, INPLACE=True):
     assert states.ndim == 2, "states must be 2D"
     assert covs.ndim == 3, "covs must be 3D"
@@ -252,8 +298,26 @@ def ukf_update_cov(states, covs, obs_noise, obsfn, obsfn_args=(), _alpha=1e-3, _
         covs -= cov_update_term
     else:
         covs = covs - cov_update_term
+    #try:
+    #    blas.dpotrf(covs, INPLACE=False)
+    #except:
+    #    print "Updated covariance is not positive definite"
+    #    from IPython import embed
+    #    embed()
+    
+    ## Compute the Cholesky decomposition
+    #chol_S = blas.dpotrf(zz_sigma_cov, False)
+    ## Select the lower triangle (set the upper triangle to zero)
+    #blas.mktril(chol_S)
+    ## Compute the determinant
+    #diag_vec = np.array([np.diag(chol_S[i]) for i in range(chol_S.shape[0])])
+    #det_S = diag_vec.prod(1)**2
+    ## Compute the inverse of the square root
+    #inv_sqrt_S = blas.dtrtri(chol_S, 'l')
     
     kalman_info.S = zz_sigma_cov
+    #kalman_info.inv_sqrt_S = inv_sqrt_S
+    #kalman_info.det_S = det_S
     kalman_info.kalman_gain = kalman_gain
     return covs, predicted_z, kalman_info
     
@@ -269,7 +333,7 @@ def np_sigma_pts(x, x_cov, _alpha=1e-3, _beta=2, _kappa=0):
     sqrt_cov = _gamma*np.linalg.cholesky(x_cov)
     
     # Array of the sigma points
-    sigma_x = np.vstack((x, x-sqrt_cov.T, x+sqrt_cov.T))
+    sigma_x = np.vstack((x, x+sqrt_cov.T, x-sqrt_cov.T))
     
     # Array of the weights for each sigma point
     wt_mn = 0.5*np.ones(1+2*_L)/(_L+_lambda)
@@ -296,6 +360,8 @@ def sigma_pts(states, covs, _alpha=1e-3, _beta=2, _kappa=0):
     # Vectors are stored row-wise and page-wise for each state-cov pair
     sigma_vecs = np.zeros((num_states, 2*state_dim+1, state_dim))
     sqrt_covs = blas.dpotrf(_L_plus_lambda*covs, INPLACE=False)
+    # Use upper triangular since we want the transpose
+    blas.mktriu(sqrt_covs)
     
     sigma_vecs[:, 0, :] = states
     sigma_vecs[:, 1:_L+1, :] = states[:, np.newaxis] + sqrt_covs

src/lib/phdfilter.py
@@ -49,14 +49,13 @@ def phd_update(weights, states, covs, filter_vars, camera, observations,
     observation_noise - numpy array of size Nx3x3
     """
     
-    log_likelihood = 1.
     num_observations = (observations.shape[0] 
                         if ( not observations.shape[1]==0) else 0)
     #z_dim = observations.shape[1]
     
     if not weights.shape[0]:
         #print "nothing to update"
-        return weights, states, covs, log_likelihood
+        return weights, states, covs
     
     detection_probability = camera.pdf_detection(states, margin=0)
     
@@ -76,11 +75,6 @@ def phd_update(weights, states, covs, filter_vars, camera, observations,
                              detection_probability[detected_indices] )
         detected_states = prev_states[detected_indices]
         detected_covs = prev_covs[detected_indices]
-        #ZZ SLAM,  step 1:
-        exp_sum__pd_predwt = np.exp(-detected_weights.sum())
-        
-        # SLAM, prep for step 2:
-        sum__clutter_with_pd_updwt = np.zeros(num_observations)
         
         if detected_weights.shape[0]:
             # Covariance update part of the Kalman update is common to all 
@@ -110,38 +104,25 @@ def phd_update(weights, states, covs, filter_vars, camera, observations,
                 
                 # Do a single covariance update if equal observation noise
                 this_observation_noise = observation_noise[0]
-                try:
-                    if not USE_UKF:
-                        _this_detected_covs_, kalman_info = kf_update_cov(
-                            detected_covs, h_mat, this_observation_noise,
-                            INPLACE=False)
+                if not USE_UKF:
+                    _this_detected_covs_, kalman_info = kf_update_cov(
+                        detected_covs, h_mat, this_observation_noise,
+                        INPLACE=False)
+                else:
+                    if USE_3D:
+                        obsfn = camera.observations
+                        obsfn_args = (1,)
                     else:
-                        if USE_3D:
-                            obsfn = camera.observations
-                            obsfn_args = (1,)
-                        else:
-                            obsfn = camera.observations_px
-                            obsfn_args = ()
-                        (_this_detected_covs_, pred_z, 
-                         kalman_info) = ukf_update_cov(
-                            detected_states, detected_covs,
-                            this_observation_noise, obsfn, obsfn_args,
-                            _alpha=1e-3, _beta=2, _kappa=0, INPLACE=False)
-                except:
-                    print "Error in kf_update_cov: Saved data to dbg"
-                    dbg = STRUCT()
-                    dbg.detected_states = detected_states
-                    dbg.detected_covs = detected_covs
-                    dbg.this_observation_noise = this_observation_noise
-                    if USE_UKF:
-                        dbg.h_mat = "not required"
-                    else:
-                        dbg.h_mat = h_mat
-                    dbg.observations = np.asarray(observations)
-                    dbg.observation_noise = np.asarray(observation_noise)
-                    print "Error in kf_update_cov"
+                        obsfn = camera.observations_px
+                        obsfn_args = ()
+                    (_this_detected_covs_, pred_z, 
+                     kalman_info) = ukf_update_cov(
+                        detected_states, detected_covs,
+                        this_observation_noise, obsfn, obsfn_args,
+                        _alpha=1e-3, _beta=2, _kappa=0, INPLACE=False)
                     
-                #this_detected_covs = _this_detected_covs_
+                    
+                this_detected_covs = _this_detected_covs_
             # We need to update the states and find the updated weights
             for obs_count in range(num_observations):
                 _observation_ = observations[obs_count]
@@ -162,35 +143,28 @@ def phd_update(weights, states, covs, filter_vars, camera, observations,
                                          #self.vars.birth_intensity +
                                          upd_weights.sum() )
                 upd_weights /= normalisation_factor
-                # SLAM, step 2:
-                sum__clutter_with_pd_updwt[obs_count] = normalisation_factor
                 
                 # Create new state with new_x and P to add to _states_
                 valid_idx = upd_weights > prune_threshold
                 if np.count_nonzero(valid_idx):
                     updated_weights += [upd_weights[valid_idx]]
                     updated_states += [upd_states[valid_idx]]
-                    updated_covs += [detected_covs[valid_idx]]
+                    updated_covs += [this_detected_covs[valid_idx]]
                 
-                if DISPLAY == 0:
+                if DISPLAY == True:
+                    print "\nUpdated weights:"
                     print upd_weights[valid_idx]
-                #    code.interact(local=locals())
-        else:
-            sum__clutter_with_pd_updwt = np.ones(num_observations)
         
         updated_weights = np.concatenate(updated_weights)
         updated_states = np.concatenate(updated_states)
         updated_covs = np.concatenate(updated_covs)
         
-        # SLAM, finalise:
-        log_likelihood = (np.log(exp_sum__pd_predwt) +
-            np.log(sum__clutter_with_pd_updwt).sum())
     except:
         print "error in update"
         exc_info = sys.exc_info()
         print "GMPHD:UPDATE():\n", traceback.print_tb(exc_info[2])
         raise
-    return updated_weights, updated_states, updated_covs, log_likelihood
+    return updated_weights, updated_states, updated_covs
 
 def phd_prune(weights, states, covs,
               prune_threshold=-1, max_num_components=-1):
@@ -417,7 +391,7 @@ def phd_iterate(weights, states,covs, filter_vars, camera, observations,
     
     # Birth
     if USE_3D:
-        (birth_wt, birth_st, birth_cv) = camera_birth( #camera_birth_disparity(
+        (birth_wt, birth_st, birth_cv) = camera_birth(
             camera, filter_vars.birth_intensity, observations, obs_noise)
     else:
         (birth_wt, birth_st, birth_cv) = camera_birth_disparity(
@@ -497,7 +471,7 @@ class GMPHD(object):
             features_pos = np.empty((0, 3))
             features_noise = np.empty((0, 3, 3))
         
-        weights, states, covs, slam_weight_log_update = phd_iterate(
+        weights, states, covs = phd_iterate(
             self.map.weights, self.map.states, self.map.covs, self.filter_vars,
             self.camera, features_pos, features_noise, USE_3D,
             USE_UKF, DISPLAY)